21833410986c9d076791d293d670502f4cd512fc
NerdOfLinux <golf@runningwind.com>
2017-08-16 13:21:46 -0500

    Create plugins.php

---
+++ plugins.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="/style.css">
+  <div id="header">
+  <title> Plugins | NerdOfLinux </title>
+  <h1 class="outline"> Plugins </h1><hr />
+    <iframe src="/navigation.html" scrolling="no"></iframe>
+  </div>
+  </head>
+ <div id="content">
+<body>
+  <p> I currently created the following plugins:
+   <ul>
+    <a href="/memberonly.html"><li> Member Only Content</li> </a>
+   </ul>
+</body>
+  </div>
+</html>
