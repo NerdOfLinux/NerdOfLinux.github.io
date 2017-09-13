4a93cbdf82c6440095bea36395eddf049d5bc592
NerdOfLinux <golf@runningwind.com>
2017-09-13 12:55:52 -0500

    Create download.sh

---
+++ tools/download.sh
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="/style.css">
+  <div id="header">
+  <title> Tools | NerdOfLinux </title>
+  <h1 class="outline"> Download a site page </h1><hr />
+    <iframe src="/navigation.html" scrolling="no"></iframe>
+  </div>
+  </head>
+ <div id="content">
+<body>
+  Coming soon...
+</body>
+  </div>
+</html>
